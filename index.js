1b3fca540881281da689fb07059a19b68e5261cf
Zinovii Dmytriv <zdmytriv@gmail.com>
2023-04-12 17:24:02 +0300

    update

---
+++ index.js
@@ -0,0 +1,28 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+    core.log('Hello World!');
+//   const token = core.getInput('token');
+//   const title = core.getInput('title');
+//   const body = core.getInput('body');
+  const octokit = github.getOctokit(token);
+  const context = github.context;
+
+  const repo = context.payload.repository.full_name;
+  const owner = context.payload.repository.owner.login;
+
+  console.log('repo', repo);
+  console.log('owner', owner);
+
+//   const response = await octokit.issues.create({
+//     owner,
+//     repo,
+//     title,
+//     body
+//   });
+
+//   core.setOutput('issue-number', response.data.number);
+} catch (error) {
+  core.setFailed(error.message);
+}
