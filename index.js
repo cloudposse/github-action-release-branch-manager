e835aff6a21140c2ea4f52907171edafb3a61250
Zinovii Dmytriv <zdmytriv@gmail.com>
2023-04-12 17:53:54 +0300

    update

--- index.js
+++ index.js
@@ -2,13 +2,11 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-    console.log('Hello World!');
 //   const token = core.getInput('token');
 //   const title = core.getInput('title');
 //   const body = core.getInput('body');
   // const octokit = github.getOctokit(token);
   const context = github.context;
-
   const repo = context.payload.repository.full_name;
   const owner = context.payload.repository.owner.login;
 
