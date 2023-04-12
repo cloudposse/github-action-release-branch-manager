b4b63c3cdd15004dcae3704563a4a121ee7ba341
Zinovii Dmytriv <zdmytriv@gmail.com>
2023-04-12 18:06:42 +0300

    update

--- index.js
+++ index.js
@@ -1,14 +1,42 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const SEMVER_PATTERN = /^(?<major>0|[1-9]\d*)\.(?<minor>0|[1-9]\d*)\.(?<patch>0|[1-9]\d*)(?:-(?<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/;
+
+class ReleaseManagerError extends Error {
+  constructor(message, status) {
+    super(message);
+    this.name = this.constructor.name;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+function validateGithubContext(context) {
+  const event_name = context.payload.event_name;
+  if (event_name != 'release') {
+    throw new ReleaseManagerError(`Unsupported event '${event_name}'. Only supported event is 'release'`);
+  }
+
+  const release_tag = get_release_tag(github_object)
+  if (!isSemver(release_tag)) {
+    throw new ReleaseManagerError(`Release tag '${release_tag}' is not in SemVer format`);
+  }
+}
+
+function get_release_tag(context) {
+  return context.payload.event.release.tag_name;
+}
+
+function isSemver(version) {
+  return SEMVER_PATTERN.test(version);
+}
+
 try {
-//   const token = core.getInput('token');
-//   const title = core.getInput('title');
 //   const body = core.getInput('body');
   // const octokit = github.getOctokit(token);
   const context = github.context;
-  const repo = context.payload.repository.full_name;
-  const owner = context.payload.repository.owner.login;
+
+  validateGithubContext(context);
 
   console.log('repo', repo);
   console.log('owner', owner);
