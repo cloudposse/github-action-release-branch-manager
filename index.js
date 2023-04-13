40dcdf190bb2fd09af90958d3f20620330b8962d
Zinovii Dmytriv <zdmytriv@gmail.com>
2023-04-13 22:03:24 +0300

    update

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const core = require('@actions/core');
 const { main } = require('./src/app.js');
 
 response = main(process.argv[2], process.argv[3]);
+console.log(response.succeeded);
 
 if (!response.succeeded) {
+    console.log(response.message);
     core.setFailed(response.message);
 }
