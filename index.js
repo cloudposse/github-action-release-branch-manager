d86771b69dec5ca58e3825380499166ae03d55e2
Zinovii Dmytriv <zdmytriv@gmail.com>
2023-04-13 22:16:47 +0300

    Added release-drafter

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const core = require('@actions/core');
 const { main } = require('./src/app.js');
 
-response = main(process.argv[2], process.argv[3]);
-console.log(response.succeeded);
-
-if (!response.succeeded) {
-    console.log(response.message);
-    core.setFailed(response.message);
-}
+main(process.argv[2], process.argv[3])
+    .then(response => {
+        if (!response.succeeded) {
+            core.setFailed(response.message);
+        }
+    })
+    .catch(error => {
+        core.setFailed(error);
+    });
