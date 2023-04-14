36ffca35bd1d3f0901dd4dd234e1ec0660e84f96
Zinovii Dmytriv <zdmytriv@gmail.com>
2023-04-14 11:28:43 +0300

    update

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const core = require('@actions/core');
 const { main } = require('./src/app.js');
+const log4js = require('log4js');
+
+log4js.configure({
+    appenders: { console: { type: 'console' } },
+    categories: { default: { appenders: ['console'], level: 'info' } }
+});
+
+const logger = log4js.getLogger();
 
 main(process.argv[2], process.argv[3])
     .then(response => {
-        if (!response.succeeded) {
+        if (response.succeeded) {
+            logger.info(response.message);
+        } else {
+            logger.error(response.message);
             core.setFailed(response.message);
         }
     })
     .catch(error => {
+        logger.error(`An error occurred: ${error}`);
         core.setFailed(error);
     });
--- src/app.js
+++ src/app.js
@@ -4,12 +4,8 @@ const log4js = require('log4js');
 const gitUtils = require('../src/git_utils.js');
 
 log4js.configure({
-  appenders: {
-    console: { type: 'console' },
-  },
-  categories: {
-    default: { appenders: ['console'], level: 'info' }
-  }
+  appenders: { console: { type: 'console' } },
+  categories: { default: { appenders: ['console'], level: 'info' } }
 });
 
 const logger = log4js.getLogger();
@@ -111,6 +107,14 @@ function readFile(contextFile) {
   });
 }
 
+function buildResponse(succeeded, reason, message) {
+  const response = new Response();
+  response.succeeded = succeeded;
+  response.reason = reason;
+  response.message = message;
+  return response;
+}
+
 async function main(workingDirectory, contextFile, doPush = true) {
   let context;
   if (contextFile != null) {
@@ -120,26 +124,18 @@ async function main(workingDirectory, contextFile, doPush = true) {
     context = github.context;
   }
 
-  const response = new Response();
-
   const eventName = context.eventName;
   if (eventName != 'release') {
-    response.succeeded = false;
-    response.reason = RESPONSE_REASON.INVALID_EVENT_TYPE;
-    response.message = `Unsupported event '${eventName}'. Only supported event is 'release'`;
-    logger.error(response.message);
-    return response;
+    return buildResponse(false, RESPONSE_REASON.INVALID_EVENT_TYPE,
+      `Unsupported event '${eventName}'. Only supported event is 'release'`);
   }
 
   const releaseTag = getReleaseTag(context)
   logger.info(`Release tag: ${releaseTag}`);
 
   if (!isSemver(releaseTag)) {
-    response.succeeded = false;
-    response.reason = RESPONSE_REASON.TAG_IS_NOT_SEMVER;
-    response.message = `Release tag '${releaseTag}' is not in SemVer format`;
-    logger.error(response.message);
-    return response;
+    return buildResponse(false, RESPONSE_REASON.TAG_IS_NOT_SEMVER,
+      `Release tag '${releaseTag}' is not in SemVer format`);
   }
 
   const targetBranch = getTargetBranch(context);
@@ -152,24 +148,17 @@ async function main(workingDirectory, contextFile, doPush = true) {
   logger.info(`Default branch: ${defaultBranch}`);
 
   if (majorForReleaseTag == '0') {
-    response.succeeded = true;
-    response.reason = RESPONSE_REASON.MAJOR_TAG_IS_0;
-    response.message = `Major version of release tag is '0'. No release branch will be created. All good.`;
-    logger.error(response.message);
-    return response;
+    return buildResponse(true, RESPONSE_REASON.MAJOR_TAG_IS_0,
+      `Major version of release tag is '0'. No release branch will be created. All good.`);
   }
 
   repoPath = workingDirectory;
 
   process.chdir(workingDirectory);
 
-  if (targetBranch == defaultBranch) {
+  if (targetBranch == defaultBranch || targetBranch == `refs/heads/${defaultBranch}`) {
     if (doesMajorTagAlreadyExist(workingDirectory, majorForReleaseTag, releaseTag)) {
-      response.succeeded = true;
-      response.reason = RESPONSE_REASON.MAJOR_TAG_ALREADY_EXISTS;
-      response.message = `Major tag '${majorForReleaseTag}' for '${releaseTag}' already exists. All good.`;
-      logger.info(response.message);
-      return response;
+      return buildResponse(true, RESPONSE_REASON.MAJOR_TAG_ALREADY_EXISTS, `Major tag '${majorForReleaseTag}' for '${releaseTag}' already exists. All good.`);
     }
 
     const previousCommit = gitUtils.getPreviousCommit(repoPath, getTagCommit(context));
@@ -180,47 +169,25 @@ async function main(workingDirectory, contextFile, doPush = true) {
     logger.info(`Release branch to create: ${releaseBranchName}`);
 
     if (gitUtils.doesBranchExist(repoPath, releaseBranchName)) {
-      response.succeeded = false;
-      response.reason = RESPONSE_REASON.RELEASE_BRANCH_ALREADY_EXISTS;
-      response.message = `Branch '${releaseBranchName}' already exists`;
-      logger.info(response.message);
-      return response;
+      return buildResponse(false, RESPONSE_REASON.RELEASE_BRANCH_ALREADY_EXISTS, `Branch '${releaseBranchName}' already exists`);
     }
 
     logger.info(`Creating branch '${releaseBranchName}' from commit '${previousCommit}' and pushing it to origin`);
     gitUtils.createBranchFromCommitAndPush(repoPath, releaseBranchName, previousCommit, doPush);
-    response.succeeded = true;
-    response.reason = RESPONSE_REASON.SUCCESSFULLY_CREATED_RELEASE_BRANCH;
-    response.message = `Created branch '${releaseBranchName}'`;
-    logger.info(response.message);
-
-    return response;
+    return buildResponse(true, RESPONSE_REASON.SUCCESSFULLY_CREATED_RELEASE_BRANCH, `Created branch '${releaseBranchName}'`);
   } else if (isBranchAReleaseBranch(targetBranch)) {
     const majorForReleaseBranch = getMajorFromReleaseBranch(targetBranch);
 
     if (majorForReleaseTag == majorForReleaseBranch) {
-      response.succeeded = true;
-      response.reason = RESPONSE_REASON.PUBLISHED_RELEASE_TO_RELEASE_BRANCH;
-      response.message = `Published release ${releaseTag} for release branch '${targetBranch}'. All good.`;
-      logger.info(response.message);
-
-      return response;
+      return buildResponse(true, RESPONSE_REASON.PUBLISHED_RELEASE_TO_RELEASE_BRANCH,
+        `Published release ${releaseTag} for release branch '${targetBranch}'. All good.`);
     } else {
-      response.succeeded = false;
-      response.reason = RESPONSE_REASON.RELEASE_TAG_AND_RELEASE_BRANCH_DOESNT_MATCH;
-      response.message = `Major version in release tag '${releaseTag}' does not match release branch version '${targetBranch}'`;
-      logger.error(response.message);
-
-      return response;
+      return buildResponse(false, RESPONSE_REASON.RELEASE_TAG_AND_RELEASE_BRANCH_DOESNT_MATCH,
+        `Major version in release tag '${releaseTag}' does not match release branch version '${targetBranch}'`);
     }
   } else {
-    response.succeeded = false;
-    response.reason = RESPONSE_REASON.TARGET_BRANCH_SHOULD_BE_EITHER_DEFAULT_OR_RELEASE_BRANCH;
-    response.message = `Target branch '${targetBranch}' is not a default or release branch`;
-
-    logger.error(response.message);
-
-    return response;
+    return buildResponse(false, RESPONSE_REASON.TARGET_BRANCH_SHOULD_BE_EITHER_DEFAULT_OR_RELEASE_BRANCH,
+      `Target branch '${targetBranch}' is not a default or release branch`);
   }
 }
 
