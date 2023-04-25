03d27c226c21999b51a4ca6919828729fbe3b01f
Zinovii Dmytriv <zdmytriv@gmail.com>
2023-04-25 15:28:23 +0300

    Updating target_commitish in release

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
-const fs = require('fs');
 const semver = require('semver');
-const github = require('@actions/github');
-const GitWrapper = require('../src/git_wrapper.js');
 const { logger } = require('./utils.js');
+const GitWrapper = require('../src/git_wrapper.js');
+const GitHubWrapper = require('../src/github_wrapper.js');
 
 const RELEASE_BRANCH_PREFIX = 'release/v';
 const RESPONSE_REASON = {
@@ -21,31 +20,6 @@ class Response {
 
 function getDefaultBranch(context) {
   return context.payload.repository.default_branch;
-}
-
-function readFile(contextFile) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(contextFile, 'utf8', (error, data) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(JSON.parse(data));
-      }
-    });
-  });
-}
-
-async function loadContext(contextFile) {
-  let context;
-
-  if (contextFile != null) {
-    const github = await readFile(contextFile);
-    context = github.context;
-  } else {
-    context = github.context;
-  }
-
-  return context;
 }
 
 function isSemver(version) {
@@ -69,11 +43,12 @@ function getLatestSemVerTagsForPerMajor(tags) {
   return latestTagsPerMajorVersion;
 }
 
-async function main(repoPath, minimalVersion, doPush = true, contextFile = null) {
+async function main(repoPath, minimalVersion, context, token, doPush = true) {
   try {
-    const context = await loadContext(contextFile);
     const defaultBranch = getDefaultBranch(context);
     const gitWrapper = new GitWrapper(repoPath);
+    const repoFullName = context.payload.repository.full_name;
+    const githubWrapper = new GitHubWrapper(token, repoFullName);
 
     const allTags = await gitWrapper.getAllTags();
     logger.debug(`All available tags:\n${allTags.join('\n')}`);
@@ -99,6 +74,8 @@ async function main(repoPath, minimalVersion, doPush = true, contextFile = null)
       const releaseBranch = `${RELEASE_BRANCH_PREFIX}${major}`;
       const releaseBranchExists = await gitWrapper.branchExists(releaseBranch);
 
+      await gitWrapper.checkout(defaultBranch);
+
       if (releaseBranchExists) {
         logger.info(`Release branch '${releaseBranch}' for major tag ${major} already exists. Skipping.`);
         continue;
@@ -119,9 +96,19 @@ async function main(repoPath, minimalVersion, doPush = true, contextFile = null)
 
       if (doPush) {
         await gitWrapper.pushToRemote(releaseBranch);
-      }
 
-      await gitWrapper.checkout(defaultBranch);
+        const release = githubWrapper.findReleaseByTag(tag);
+
+        if (release != null) {
+          if (release.target_commitish !== releaseBranch) {
+            await githubWrapper.updateTargetCommitish(release.id, releaseBranch);
+          } else {
+            logger.info(`Release for tag (${tag}) already exists and has correct target_commitish (${releaseBranch}).`);
+          }
+        } else {
+          await githubWrapper.createReleaseForTag(tag, releaseBranch);
+        }
+      }
 
       responseData[releaseBranch] = tag;
 
